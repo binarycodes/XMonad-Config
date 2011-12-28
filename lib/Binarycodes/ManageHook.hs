64f50724f27b6eed7bb8bb1ad333bbec944f8bcf
Sujoy Kumar Das <sujoy@archlinux.us>
2011-12-29 00:05:04 +0530

    composeOne allows to swapDown windows without swapping down dialog windows

--- lib/Binarycodes/ManageHook.hs
+++ lib/Binarycodes/ManageHook.hs
@@ -22,24 +22,29 @@ import Binarycodes.Workspaces
 manageHook' :: ManageHook
 manageHook' = myManageHook <+> manageHook defaultConfig <+> manageDocks
 
-myManageHook = composeAll $ concat
-             [ [ stringProperty "WM_WINDOW_ROLE" =? roleC --> doIgnore | roleC <- hide ]
-             , [ className =? webC --> doF (W.shift $ getWorkspaceId "web")  | webC <- web ]
-             , [ className =? officeC --> doF (W.shift $ getWorkspaceId "office") | officeC <- office ]
-             , [ className =? mailC --> doF (W.shift $ getWorkspaceId "mail") | mailC <- mail ]
-             , [ className =? docC --> doF (W.shift $ getWorkspaceId "doc")  | docC <- doc ]
-             , [ className =? codeC --> doF (W.shift $ getWorkspaceId "code") | codeC <- code ]
-             , [ className =? chatC --> doF (W.shift $ getWorkspaceId "chat") | chatC <- chat ]
-             , [ className =? floatC --> doF (W.shift $ getWorkspaceId "float") | floatC <- float ]
-             , [ className =? downC --> doF (W.shift $ getWorkspaceId "down") | downC <- down ]
+myManageHook = composeOne $ concat
+             [ [ stringProperty "WM_WINDOW_ROLE" =? roleC -?> doIgnore | roleC <- hide ]
+             , [ isDialog -?> doFloat ]
+             , [ className =? webC -?> doF (W.shift $ getWorkspaceId "web")  | webC <- web ]
+             , [ className =? officeC -?> doF (W.shift $ getWorkspaceId "office") | officeC <- office ]
+             , [ className =? multC -?> doF (W.shift $ getWorkspaceId "mult") | multC <- mult ]
+             , [ className =? docC -?> doF (W.shift $ getWorkspaceId "doc")  | docC <- doc ]
+             , [ className =? codeC -?> doF (W.shift $ getWorkspaceId "code") | codeC <- code ]
+             , [ className =? chatC -?> doF (W.shift $ getWorkspaceId "chat") | chatC <- chat ]
+             , [ className =? floatC -?> doF (W.shift $ getWorkspaceId "float") | floatC <- float ]
+             , [ className =? downC -?> doF (W.shift $ getWorkspaceId "down") | downC <- down ]
+             , [ className =? sinkC -?> unFloat | sinkC <- sink ]
+             , [ return True -?> doF W.swapDown ]
              ]
-             where web  = [ "Namoroka", "Jumanji", "Opera", "Firefox", "Chromium", "Kazehakase" ]
+             where 
+                   unFloat = ask >>= doF . W.sink
+                   web  = [ "Namoroka", "Jumanji", "Opera", "Firefox", "Chromium", "Kazehakase" ]
                    doc  = [ "GV" ,"Evince", "Xchm", "Epdfview", "Zathura", "Chmsee" ]
                    code = [ "Netbeans" ]
                    chat = [ "Pidgin","Qq" ]
                    float = [ "Gimp" , "Blender" ]
-                   mail = [ "Lanikai", "Liferea-bin" ]
+                   mult = [ "Shotwell" ]
                    down = [ "Transmission", "Deluge" ]
                    office = [ "libreoffice-calc", "libreoffice-startcenter", "LibreOffice 3.3" ]
                    hide = [ ]
-
+                   sink = [ ]
